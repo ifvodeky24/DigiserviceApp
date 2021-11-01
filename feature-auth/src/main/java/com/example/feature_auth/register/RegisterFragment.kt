1b1ddfbf27dd9da9cd8e95f6e7485863736ec371
Ridho Afni <ridho.afni@bithealth.co.id>
2021-11-01 22:41:57 +0700

    Update

--- feature-auth/src/main/java/com/example/feature_auth/register/RegisterFragment.kt
+++ feature-auth/src/main/java/com/example/feature_auth/register/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.feature_auth.R
 import com.example.feature_auth.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
@@ -19,6 +21,17 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupInput()
+    }
+
+    private fun setupInput() {
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.chooseFragment)
+        }
     }
 
     override fun onDestroyView() {
