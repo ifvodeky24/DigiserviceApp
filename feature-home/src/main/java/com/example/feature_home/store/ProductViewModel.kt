01756d67d0b97cd34bcfb225f745efa6558eaf58
Ridho Afni <ridho.afni@bithealth.co.id>
2021-11-22 19:47:08 +0530

    Update

--- feature-home/src/main/java/com/example/feature_home/store/ProductViewModel.kt
+++ feature-home/src/main/java/com/example/feature_home/store/ProductViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.feature_home.store
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -7,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.core_data.api.ApiEvent
 import com.example.core_data.domain.store.ListProductGetAll
 import com.example.core_data.repository.StoreRepository
+import com.example.core_util.Constants
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
@@ -14,6 +16,21 @@ import kotlinx.coroutines.launch
 class ProductViewModel(
     private val storeRepository: StoreRepository
 ) : ViewModel() {
+
+    var firstImagePath = Constants.EMPTY_STRING
+    var secImagePath = Constants.EMPTY_STRING
+    var thirdImagePath = Constants.EMPTY_STRING
+    var fourthImagePath = Constants.EMPTY_STRING
+    var fifthImagePath = Constants.EMPTY_STRING
+
+    var firstImageUri: Uri? = null
+    var secImageUri: Uri? = null
+    var thirdImageUri: Uri? = null
+    var fourthImageUri: Uri? = null
+    var fifthImageUri: Uri? = null
+
+    var flagChoose = Constants.EMPTY_STRING
+
 
     private val _productGetAllResponse = MutableLiveData<ApiEvent<ListProductGetAll?>>()
     val productGetAllResponse: LiveData<ApiEvent<ListProductGetAll?>> = _productGetAllResponse
