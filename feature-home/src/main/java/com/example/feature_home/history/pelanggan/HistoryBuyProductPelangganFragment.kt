0f25b153b3f20df27cd6a50790761d256c0ac847
abdulhafizramadan <20104069@ittelkom-pwt.ac.id>
2021-11-28 22:08:16 +0700

    Handle update status pembelian barang untuk pelanggan

--- feature-home/src/main/java/com/example/feature_home/history/pelanggan/HistoryBuyProductPelangganFragment.kt
+++ feature-home/src/main/java/com/example/feature_home/history/pelanggan/HistoryBuyProductPelangganFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import com.afollestad.recyclical.datasource.dataSourceTypedOf
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
@@ -14,8 +15,10 @@ import com.example.core_data.domain.store.ProductBuyHistoryGetAll
 import com.example.feature_home.R
 import com.example.feature_home.account.AccountViewModel
 import com.example.feature_home.databinding.FragmentHistoryServicePelangganBinding
+import com.example.feature_home.history.teknisi.HistoryBuyProductTeknisiFragment
 import com.example.feature_home.store.ProductViewModel
 import com.example.feature_home.viewHolder.ItemHistoryBuyProduct
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -99,8 +102,25 @@ class HistoryProductPelangganFragment : Fragment() {
                             .load(item.pathPhoto)
                             .into(ivProductPhoto)
 
+                        if (item.beliStatus != "booking") {
+                            buttonActionContainer.visibility = View.GONE
+                        }
+
                         btnGiveReview.setOnClickListener {
 
+                        }
+
+                        btnProductCancel.setOnClickListener {
+                            showAlertDialog("Apakah kamu yang ingin membatalkan traksaksi ini?", "Batal", "Ya") {
+                                updateStatusBeliProduk(item.beliId, HistoryBuyProductTeknisiFragment.Companion.StatusBeliProduk.Cancel())
+                            }
+
+                        }
+
+                        btnProductFinish.setOnClickListener {
+                            showAlertDialog("Apakah kamu yang ingin menyelesaikan traksaksi ini?", "Batal", "Ya") {
+                                updateStatusBeliProduk(item.beliId, HistoryBuyProductTeknisiFragment.Companion.StatusBeliProduk.Finish())
+                            }
                         }
                     }
                 }
@@ -108,10 +128,59 @@ class HistoryProductPelangganFragment : Fragment() {
         }
     }
 
+    private fun updateStatusBeliProduk(beliId: Int, status: HistoryBuyProductTeknisiFragment.Companion.StatusBeliProduk) {
+        productViewModel.updateStatusBeliProduct(beliId, status.status)
+
+        productViewModel.updateStatusBeliProduct.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is ApiEvent.OnProgress -> {}
+                is ApiEvent.OnSuccess -> {
+                    observeUser()
+                    val snackbarMessage = if (status.status == HistoryBuyProductTeknisiFragment.Companion.StatusBeliProduk.Cancel().status) {
+                        "Pemesanan berhasil dibatalkan!"
+                    } else {
+                        "Produk telah diterima, terima kasih telah berbelanja disini!"
+                    }
+                    Snackbar.make(requireContext(), requireView(), snackbarMessage, Snackbar.LENGTH_SHORT).show()
+                }
+                is ApiEvent.OnFailed -> {
+                    Snackbar.make(requireContext(), requireView(), "Update status pembelian gagal, mohon coba lagi!", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    private fun showAlertDialog(
+        title: String,
+        negativeButtonTitle: String,
+        positiveButtonTitle: String,
+        onPositiveButtonClick: () -> Unit
+    ) {
+        val alertDialog = AlertDialog.Builder(requireContext())
+            .setTitle(title)
+            .setNegativeButton(negativeButtonTitle) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton(positiveButtonTitle) { dialog, _ ->
+                onPositiveButtonClick()
+                dialog.dismiss()
+            }
+            .create()
+        return alertDialog.show()
+    }
 
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    companion object {
+        sealed class StatusBeliProduk(val status: String) {
+            class Cancel : StatusBeliProduk("dibatalkan")
+            class Finish : StatusBeliProduk("selesai")
+        }
+    }
+
 }
