a0a7db88f4c88c2dd1b73980a325053b3dd17be9
abdulhafizramadan <20104069@ittelkom-pwt.ac.id>
2021-11-30 11:12:11 +0700

    Handle when user click btn submit, if data data success store. user will redirect to home login

--- feature-auth/src/main/java/com/example/feature_auth/choose/ChooseFragment.kt
+++ feature-auth/src/main/java/com/example/feature_auth/choose/ChooseFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.afollestad.recyclical.datasource.dataSourceTypedOf
 import com.afollestad.recyclical.setup
@@ -18,6 +19,7 @@ import com.example.core_data.domain.JenisKerusakan
 import com.example.core_data.domain.ListJenisKerusakan
 import com.example.feature_auth.R
 import com.example.feature_auth.databinding.FragmentChooseBinding
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChooseFragment : Fragment() {
@@ -39,6 +41,7 @@ class ChooseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeData()
+        observeWhenDataSaved()
         setInput()
     }
 
@@ -148,6 +151,22 @@ class ChooseFragment : Fragment() {
             }
         }
     }
+
+    private fun observeWhenDataSaved() {
+        chooseViewModel.chooseServiceResponse.observe(viewLifecycleOwner) { event ->
+            when (event) {
+                is ApiEvent.OnProgress -> {}
+                is ApiEvent.OnSuccess -> {
+                    Snackbar.make(requireContext(), requireView(), "Berhasil menyimpan keahlian, mohon login untuk memasukin halaman utama!", Snackbar.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.loginFragment)
+                }
+                is ApiEvent.OnFailed -> {
+                    Snackbar.make(requireContext(), requireView(), "Gagal menyimpan keahlian, mohon coba lagi!", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
